1c0e4a31844390c762d5271c1aac515d1399e22b
SaySohail <sohailpeerzade@gmail.com>
2023-11-30 20:06:27 +0000

    updating port

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.post('/process-question', async (req, res) => {
 });
 
 // Set the port
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
